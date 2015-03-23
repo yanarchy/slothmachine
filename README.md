473860acaaf1d5ce6e53ca2fe1fe4f7a5e6b429a
Yan Fan <theyanfan@gmail.com>
2015-03-23 11:27:41 -0700

    Add initial files, add source ribbon.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# slothmachine
+# Espresso Bar Slot Machine
+
+### What will you be having today?
+
+#### Press the button and try your luck!
+
+## Built with just the basics:
+
+* JavaScript
+* jQuery
+* HTML & CSS
+* And a little bit of Photoshop
+
---
+++ js/machine.js
@@ -0,0 +1,8 @@
+// Top-right ribbon that links to GitHub code
+$('body').append(
+  '<a href="https://github.com/yanarchy/slothmachine"> \
+  <img style="position: fixed; top: 0; right: 0; border: 0;" \
+  src="images/sourceribbon.png" \
+  alt="See the source code" > \
+  </a>'
+);
